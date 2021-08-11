9c1eff7bd0337f87855745befbc9f4cd33baf9e2
blossun <blossunn@gmail.com>
2021-08-11 16:40:02 +0900

    test: ProductServiceTest 구현 - with. 가짜 객체
    목 객체가 아닌 가짜 객체를 사용해서 테스트코드를 작성

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -3,10 +3,19 @@ package kitchenpos.application;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
-import kitchenpos.fixture.ProductFixture;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class ProductServiceTest {
     private ProductRepository productRepository = new InMemoryProductRepository();
@@ -23,14 +32,44 @@ class ProductServiceTest {
         productService = new ProductService(productRepository, menuRepository, purgomalumClient);
     }
 
+    @DisplayName("상품을 등록할 수 있다.")
     @Test
     void create() {
-        //given
-        Product product = ProductFixture.generateProduct();
+        final Product expected = new Product();
+        expected.setName("후라이드");
+        expected.setPrice(BigDecimal.valueOf(16_000L));
 
-        //when
-        productService.create(product);
+        final Product actual = productService.create(expected);
+        assertThat(actual).isNotNull();
+        assertAll(
+                () -> assertThat(actual.getId()).isNotNull(),
+                () -> assertThat(actual.getName()).isEqualTo(expected.getName()),
+                () -> assertThat(actual.getPrice()).isEqualTo(expected.getPrice())
+        );
+    }
 
-        //then
+    @DisplayName("상품의 가격이 올바르지 않으면 등록할 수 없다.")
+    @ValueSource(strings = "-1000")
+    @NullSource
+    @ParameterizedTest
+    void create(final BigDecimal price) {
+        final Product expected = new Product();
+        expected.setName("후라이드");
+        expected.setPrice(price);
+
+        assertThatThrownBy(() -> productService.create(expected))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품의 이름이 올바르지 않으면 등록할 수 없다.")
+    @ValueSource(strings = {"비속어", "욕설이 포함된 이름"})
+    @ParameterizedTest
+    void create(final String name) {
+        final Product expected = new Product();
+        expected.setName(name);
+        expected.setPrice(BigDecimal.valueOf(16_000L));
+
+        assertThatThrownBy(() -> productService.create(expected))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
